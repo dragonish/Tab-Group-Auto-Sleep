4f31d7a57a82fac71ea18a1e124794e57f31381c
dragonish <no.web.developer@outlook.com>
2026-03-04 14:52:37 +0800

    init: repository initialisation

---
+++ vite.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import path from 'node:path';
+import HTMLLocation from 'rollup-plugin-html-location';
+
+const productionMode = process.env.NODE_ENV === 'production';
+const modeDir = productionMode ? 'build' : 'dist';
+
+const htmlNames = [];
+const jsNames = ['background'];
+const pages = {};
+htmlNames.forEach(name => {
+  pages[name] = path.resolve(__dirname, `src/${name}/index.html`);
+});
+jsNames.forEach(name => {
+  pages[name] = path.resolve(__dirname, `src/${name}/index.ts`);
+});
+const outDir = `${path.resolve(__dirname, modeDir)}`;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    outDir,
+    sourcemap: !productionMode,
+    chunkSizeWarningLimit: 1024,
+    rollupOptions: {
+      input: pages,
+      output: {
+        entryFileNames: '[name]/index.js',
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  publicDir: 'src/assets',
+  plugins: [
+    HTMLLocation({
+      filename: input => input.replace('src/', ''),
+    }),
+  ],
+});
