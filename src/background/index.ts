9d26ff1b9dd681f45725fda6bdbd2ee828d65be2
dragonish <no.web.developer@outlook.com>
2026-03-04 19:10:34 +0800

    feat(background): implement function

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,3 +1,24 @@
-console.log('background');
+function discardTab(tab: chrome.tabs.Tab) {
+  if (tab.id && tab.groupId > -1 && !tab.active && !tab.discarded && !tab.frozen) {
+    chrome.tabs.discard(tab.id);
+  }
+}
 
-export {};
+chrome.tabs.onCreated.addListener(tab => {
+  try {
+    discardTab(tab);
+  } catch {
+    // do nothing
+  }
+});
+
+chrome.tabs.onUpdated.addListener(async (tabId, changeInfo) => {
+  if (changeInfo.status === 'loading') {
+    try {
+      const tab = await chrome.tabs.get(tabId);
+      discardTab(tab);
+    } catch {
+      // do nothing
+    }
+  }
+});
